1625808bc362394d35844ec875636d6d55ec8a28
Shelby <shelbyferrier1@gmail.com>
2022-06-13 17:00:38 -0700

    style, remove yellow letters from possible letters

--- wordle_solver.py
+++ wordle_solver.py
@@ -8,112 +8,131 @@ It's likely this could be improved by minimizing the average length of game inst
 It should be noted that we are using the wordle answer bank for the possible guesses, for simplicity. In reality 
 there are about 10,000 more possible guesses.
 """
+from logging import exception
 import numpy as np
 import random
 import string
-import csv
 
-wordList = np.loadtxt("wordle-answers.txt", dtype=str)
+word_list = np.loadtxt("wordle-answers.txt", dtype=str)
 
 class Game:
-    def __init__(self, answer, guessed, posGuesses = wordList, statusUpdates = True):
+    def __init__(self, answer, guessed, pos_guesses = word_list, status_updates = True):
         self.answer = answer
-        self.statusUpdates = statusUpdates
+        self.status_updates = status_updates # prints the progress of the game through the time intensive method update_best_guess()
         self.guessed = guessed
-        self.posGuesses = posGuesses
+        self.k_guessed  = 0
+        self.pos_guesses = pos_guesses
         self.yellows = set()
-        self.posLetters = []
-        self.bestGuess = ""
-        for i in range(5):
-            self.posLetters.append(list(string.ascii_lowercase))
-        
+        self.pos_letters = [list(string.ascii_lowercase)]* 5
+        self.best_guess = ""
     """ 
-    A method that returns the best guess
+    returns the guess that minimizes the average number of guesses remaining, 
+    as well as a list of all possible guess with their averages
     """
-    def updateBestGuess(self):
+    def update_best_guess(self):
         avgs = []
-        if len(self.guessed) > 0:
-            self.update()
-        bestAvg = np.inf
+        best_avg = np.inf
         i = 0
-        for g in self.posGuesses:
+        # iterate over possible guesses
+        for g in self.pos_guesses:
             i += 1 
-            currentAvg = 0
-            currentAvg = self.testGuess(g)
-            avgs.append([g, currentAvg])
-            if self.statusUpdates and i % 10 == 0: print("progress: ", i , "/ ", len(self.posGuesses))
-            if currentAvg < bestAvg:
-                bestAvg = currentAvg
-                self.bestGuess = g
-        if self.statusUpdates: print("Best guess: ", self.bestGuess, "- avg:", bestAvg)
-        return (self.bestGuess, avgs)
+            # get average number of guesses to finish the game
+            current_avg = self.test_guess(g)
+            avgs.append([g, current_avg])
+            if self.status_updates and i % 10 == 0: print("progress: ", i , "/ ", len(self.pos_guesses))
+            # check if this is the best guess so far
+            if current_avg < best_avg:
+                best_avg = current_avg
+                self.best_guess = g
+        if self.status_updates: print("Next guess: ", self.best_guess, "- avg:", best_avg)
+        return (self.best_guess, avgs)
 
     """
-    updates posLetters and posGuesses
+    updates pos_letters and pos_guesses based on the words in guessed
     """
     def update(self):
-        # update posLetters based off of changes in guessed
-        for guess in self.guessed:
+        # update pos_letters
+        for guess in self.guessed[self.k_guessed: len(self.guessed)]:
             for i in range(5):
                 if guess[i] is self.answer[i]: # green
-                    self.posLetters[i] = list(guess[i])
-                elif(guess[i] in self.answer): # yellow
-                    self.yellows.add(guess[i])
-                else:                          # gray
-                    for j in range(5): 
-                        if guess[i] in self.posLetters[j]:
-                            self.posLetters[j].remove(guess[i])
-        
-        # update posGuesses off of changes in posLetters
+                    self.pos_letters[i] = list(guess[i])
+                elif(guess[i] in self.answer):   
+                    self.yellows.add(guess[i]) # update yellow word bank
+                    # remove yellow
+                    if guess[i] in self.pos_letters[i]:
+                        self.pos_letters[i] = [letter for letter in self.pos_letters[i] if letter != guess[i]]
+                else:
+                    # remove grays
+                    for j in range(5):         
+                        if guess[i] in self.pos_letters[j]:
+                            self.pos_letters[j].remove(guess[i])
+        # update pos_guesses
         newWordBank = set()
-        if len(self.posGuesses) == 0:
-            return
-        for posGuess in self.posGuesses:
-            sheValid = True
+        for pos_guess in self.pos_guesses:
+            # add g to pos_guesses if it meets the conditions in pos_letters and yellows
+            valid_guess = True
             for i in range(5):
-                if posGuess[i] not in self.posLetters[i]:
-                    sheValid = False
+                if pos_guess[i] not in self.pos_letters[i]:
+                    valid_guess = False
             for yellow in self.yellows:
-                if yellow not in posGuess:
-                    sheValid = False
-            if sheValid:
-                newWordBank.add(posGuess)
-        self.posGuesses = list(newWordBank)
-    
+                if yellow not in pos_guess:
+                    valid_guess = False
+            if valid_guess:
+                newWordBank.add(pos_guess)
+        self.pos_guesses = list(newWordBank)
+        self.k_guessed = len(self.guessed)
+        if len(self.pos_guesses == 0):
+            raise Exception("There are no valid guesses remaining")
     
     """
-    returns an average number of possible guesses remaining after guessing this word
+    tests the eficacy of a new guess in this game
+    returns the average number of possible guess remaining
     """
-    def testGuess(self, guess):
+    def test_guess(self, guess):
         avgGuesses = 0
         newGuesses = self.guessed.copy()
-        newGuesses.append(guess)
-        for pretendAnswer in self.posGuesses:
-            pretendGame = Game(pretendAnswer, newGuesses, posGuesses = self.posGuesses)
-            pretendGame.update()
-            avgGuesses += len(pretendGame.posGuesses) / len(self.posGuesses)
+        newGuesses.append(guess) # a list of all guesses made, including the one we're testing
+        for possible_answer in self.pos_guesses: # iterate over possible winning words
+            # creates a mini game with guess included as the next guess
+            pretend_game = Game(possible_answer, newGuesses)
+            pretend_game.update()
+            avgGuesses += len(pretend_game.pos_guesses) / len(self.pos_guesses)
         return avgGuesses
-    
-    def guessNewWord(self, guess):
+
+    def guess_new_word(self, guess):
         self.guessed.append(guess)
+        self.update()
     
-    def playBall(self):
-        while self.bestGuess != self.answer:
-            self.updateBestGuess()
-            self.guessNewWord(self.bestGuess)
-        if self.statusUpdates:
+    """
+    gets number of guesses taken by the program to get the answer
+    """
+    def play_ball(self):
+        self.update()
+        # until the answer is guessed, keep guessing things
+        while self.best_guess != self.answer:
+            self.update_best_guess()
+            self.guess_new_word(self.best_guess)
+        if self.status_updates:
             print(len(self.guessed), " tries to guess ", self.answer)
         return len(self.guessed)
 
-def selfEval():
-    avgGameLength = 0
-    wordListSample = random.sample(list(wordList), 100)
+""""
+evaluates the average game length based on a list of initial guesses
+""""
+def self_eval(init_guesses):
+    avg_game_length = 0
+    word_listSample = random.sample(list(word_list), 100)
     i = 0
-    for word in wordListSample:
+    for word in word_listSample:
         i += 1
-        game = Game(word, ["raise"])
-        avgGameLength += game.playBall()
-        print("AVG: ", avgGameLength/ i)
-    print("FINAL AVG: ", avgGameLength / 100)
+        game = Game(word, init_guesses)
+        avg_game_length += game.play_ball()
+        print("AVG: ", avg_game_length/ i)
+    print("FINAL AVG: ", avg_game_length / 100)
 
-selfEval()
+# uncomment this to run the program on the best starting word "raise"
+#self_eval(["raise"])
+
+# play ball example
+#game = Game("abase", ["house"])
+#game.play_ball()
